9cf90ed0da44084fe59a012a963dd7a19bd9b610
Mudasirrr <hmudasir714@gmail.com>
2020-06-10 11:18:01 +0500

    update Footer file

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -41,7 +41,7 @@ class Footer extends React.Component {
                   <li className="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02"><i className="fa fa-twitter"></i></a></li>
                   <li className="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02"><i className="fa fa-instagram"></i></a></li>
                   <li className="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02"><i className="fa fa-google-plus"></i></a></li>
-                  <li className="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02" target="_blank"><i className="fa fa-envelope"></i></a></li>
+                  <li className="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02"><i className="fa fa-envelope"></i></a></li>
                 </ul>
               </div>
             </div>
@@ -49,7 +49,7 @@ class Footer extends React.Component {
           <div className="row">
             <div className="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-2 text-center text-white">
               <p><u><a href="https://www.nationaltransaction.com/">National Transaction Corporation</a></u> is a Registered MSP/ISO of Elavon, Inc. Georgia [a wholly owned subsidiary of U.S. Bancorp, Minneapolis, MN]</p>
-              <p className="h6">© All right Reversed.<a className="text-green ml-2" href="https://www.sunlimetech.com" target="_blank">Sunlimetech</a></p>
+              <p className="h6">© All right Reversed.<a className="text-green ml-2" href="https://www.sunlimetech.com" >Sunlimetech</a></p>
             </div>
           </div>
       </div>
