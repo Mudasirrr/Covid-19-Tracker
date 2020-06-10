120a2d0aa3f8bfc12a2370181d7c8f78f1c416e2
Mudasirrr <hmudasir714@gmail.com>
2020-06-10 09:30:19 +0500

    Updated Initially

---
+++ README.md
@@ -0,0 +1,16 @@
+# COVID-19 Tracker
+![COVID-19 Tracker]()
+
+### [Live Demo](covid-19_tracker-by-mudasir.surge.sh)
+
+## Introduction
+In this Project, we will create a full COVID-19 Tracker. We're going to use React, Charts.JS and Material UI.
+
+
+API used: https://covid19.mathdro.id/api
+
+Setup:```
+- To install Node module on your local repository run: 
+npm i or npm install or yarr i
+- To check this app run npm start or yarn start
+```
---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React from 'react';
+
+import { Cards, CountryPicker, Chart } from './components';
+import { fetchData } from './api/';
+import styles from './App.module.css';
+
+import image from './images/image.jpeg';
+import { Grid } from '@material-ui/core';
+
+
+class App extends React.Component {
+  state = {
+    data: {},
+    country: '',
+  }
+
+  async componentDidMount() {
+    const data = await fetchData();
+
+    this.setState({ data });
+  }
+
+  handleCountryChange = async (country) => {
+    const data = await fetchData(country);
+
+    this.setState({ data, country: country });
+  }
+
+  render() {
+    const { data, country } = this.state;
+
+    return (
+      
+      <div className={styles.container}>
+        <img className={styles.image} src={image} alt="COVID-19" />
+        <h1 className={styles.header1}>Welcome to the Covid-19 Tracker <br/> Here You Can check the Updated result of Covid-19</h1>
+
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
+        <h1 className={styles.header2} >Here You can check the Result!</h1>
+        <Cards data={data} />
+        <h1 className={styles.header1}>Here ou can check chart</h1>
+
+        <Chart data={data} country={country} /> 
+      </div>
+    );
+  }
+}
+
+export default App;
