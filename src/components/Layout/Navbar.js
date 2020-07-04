674b1e46338d0a8b5315008dd49bc290f610bcf1
Mudasirrr <hmudasir714@email.com>
2020-07-05 02:52:58 +0500

    Setting for mobile screen

--- src/components/Layout/Navbar.js
+++ src/components/Layout/Navbar.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
 
-import styles from '../../App.module.css';
 export default class Navbar extends Component {
     render() {
         return (
